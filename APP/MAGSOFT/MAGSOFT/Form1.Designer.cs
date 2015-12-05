47e94518a9f9e112e722a5b71453992fff0a798e
mswiders1 <mat.swiderski@gmail.com>
2015-12-05 13:46:34 +0100

    Zaktualizowane klasy programu

--- APP/MAGSOFT/MAGSOFT/Form1.Designer.cs
+++ APP/MAGSOFT/MAGSOFT/Form1.Designer.cs
@@ -32,8 +32,8 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.ListaKlientowMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ListaKontrahentowMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.ZamknijMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.oProgramieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ZamknijMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,7 +48,7 @@
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
-            this.menuStrip1.Size = new System.Drawing.Size(97, 472);
+            this.menuStrip1.Size = new System.Drawing.Size(98, 472);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -57,7 +57,7 @@
             this.ListaKlientowMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("ListaKlientowMenuItem.Image")));
             this.ListaKlientowMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.ListaKlientowMenuItem.Name = "ListaKlientowMenuItem";
-            this.ListaKlientowMenuItem.Size = new System.Drawing.Size(84, 95);
+            this.ListaKlientowMenuItem.Size = new System.Drawing.Size(85, 95);
             this.ListaKlientowMenuItem.Text = "Lista klientów";
             this.ListaKlientowMenuItem.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.ListaKlientowMenuItem.Click += new System.EventHandler(this.ListaKlientowMenuItem_Click);
@@ -72,6 +72,15 @@
             this.ListaKontrahentowMenuItem1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.ListaKontrahentowMenuItem1.Click += new System.EventHandler(this.ListaKontrahentowMenuItem1_Click);
             // 
+            // oProgramieToolStripMenuItem
+            // 
+            this.oProgramieToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("oProgramieToolStripMenuItem.Image")));
+            this.oProgramieToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.oProgramieToolStripMenuItem.Name = "oProgramieToolStripMenuItem";
+            this.oProgramieToolStripMenuItem.Size = new System.Drawing.Size(85, 95);
+            this.oProgramieToolStripMenuItem.Text = "O Programie";
+            this.oProgramieToolStripMenuItem.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
             // ZamknijMenuItem
             // 
             this.ZamknijMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("ZamknijMenuItem.Image")));
@@ -81,15 +90,6 @@
             this.ZamknijMenuItem.Text = "Zamknij";
             this.ZamknijMenuItem.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.ZamknijMenuItem.Click += new System.EventHandler(this.ZamknijMenuItem_Click);
-            // 
-            // oProgramieToolStripMenuItem
-            // 
-            this.oProgramieToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("oProgramieToolStripMenuItem.Image")));
-            this.oProgramieToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.oProgramieToolStripMenuItem.Name = "oProgramieToolStripMenuItem";
-            this.oProgramieToolStripMenuItem.Size = new System.Drawing.Size(88, 95);
-            this.oProgramieToolStripMenuItem.Text = "O Programie";
-            this.oProgramieToolStripMenuItem.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             // 
             // GlowneOkno
             // 
@@ -104,7 +104,7 @@
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "GlowneOkno";
-            this.Text = "MAG-SOFT";
+            this.Text = "Video4You";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
---
+++ APP/MAGSOFT/MAGSOFT/Klasy/Film.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAGSOFT.Klasy
+{
+    class Film
+    {
+        string _TytulOrg;
+        string _TytulPol;
+        int _dlugoscFilmu;
+        DateTime _RokProdukcji;
+        string _Obsada;
+        string _Gatunek;
+        Ocena _OcenaFimu;
+        DateTime _CzasDodania;
+        string _Rezyser;
+        string _Opis;
+        string _KrajPoch;
+        string _URLFilmu;
+        string _Scenariusz;
+
+
+    }
+}
