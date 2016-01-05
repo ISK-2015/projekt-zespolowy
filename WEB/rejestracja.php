f1d7351c6b317722cd05384d82640076c358f137
hornowski92 <marcin_hornowski@wp.pl>
2016-01-05 23:16:36 +0100

    update

---
+++ WEB/rejestracja.php
@@ -0,0 +1,198 @@
+<?php
+
+	session_start();
+	
+	if (isset($_POST['email']))
+	{
+		//Udana walidacja? Załóżmy, że tak!
+		$wszystko_OK=true;
+		
+		//Sprawdź poprawność nickname'a
+		$nick = $_POST['nick'];
+		
+		//Sprawdzenie długości nicka
+		if ((strlen($nick)<3) || (strlen($nick)>20))
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_nick']="Nick musi posiadać od 3 do 20 znaków!";
+		}
+		
+		if (ctype_alnum($nick)==false)
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_nick']="Nick może składać się tylko z liter i cyfr (bez polskich znaków)";
+		}
+		
+		// Sprawdź poprawność adresu email
+		$email = $_POST['email'];
+		$emailB = filter_var($email, FILTER_SANITIZE_EMAIL);
+		
+		if ((filter_var($emailB, FILTER_VALIDATE_EMAIL)==false) || ($emailB!=$email))
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_email']="Podaj poprawny adres e-mail!";
+		}
+		
+		//Sprawdź poprawność hasła
+		$haslo1 = $_POST['haslo1'];
+		$haslo2 = $_POST['haslo2'];
+		
+		if ((strlen($haslo1)<8) || (strlen($haslo1)>20))
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_haslo']="Hasło musi posiadać od 8 do 20 znaków!";
+		}
+		
+		if ($haslo1!=$haslo2)
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_haslo']="Podane hasła nie są identyczne!";
+		}	
+
+		$haslo_hash = password_hash($haslo1, PASSWORD_DEFAULT);
+		
+		//Czy zaakceptowano regulamin?
+		if (!isset($_POST['regulamin']))
+		{
+			$wszystko_OK=false;
+			$_SESSION['e_regulamin']="Potwierdź akceptację regulaminu!";
+		}				
+		
+		
+		
+	
+		
+				
+		
+		//Zapamiętaj wprowadzone dane
+		$_SESSION['fr_nick'] = $nick;
+		$_SESSION['fr_email'] = $email;
+		$_SESSION['fr_haslo1'] = $haslo1;
+		$_SESSION['fr_haslo2'] = $haslo2;
+		if (isset($_POST['regulamin'])) $_SESSION['fr_regulamin'] = true;
+		
+		require_once "connect.php";
+		mysqli_report(MYSQLI_REPORT_STRICT);
+		
+		try 
+		{
+			$polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
+			if ($polaczenie->connect_errno!=0)
+			{
+				throw new Exception(mysqli_connect_errno());
+			}
+			else
+			{
+				//Czy email już istnieje?
+				$rezultat = $polaczenie->query("SELECT id FROM uzytkownicy WHERE email='$email'");
+				
+				if (!$rezultat) throw new Exception($polaczenie->error);
+				
+				$ile_takich_maili = $rezultat->num_rows;
+				if($ile_takich_maili>0)
+				{
+					$wszystko_OK=false;
+					$_SESSION['e_email']="Istnieje już konto przypisane do tego adresu e-mail!";
+				}		
+
+				//Czy nick jest już zarezerwowany?
+				$rezultat = $polaczenie->query("SELECT id FROM uzytkownicy WHERE user='$nick'");
+				
+				if (!$rezultat) throw new Exception($polaczenie->error);
+				
+				$ile_takich_nickow = $rezultat->num_rows;
+				if($ile_takich_nickow>0)
+				{
+					$wszystko_OK=false;
+					$_SESSION['e_nick']="Istnieje już takim nicku! Wybierz inny.";
+				}
+				
+				if ($wszystko_OK==true)
+				{
+					//Hurra, wszystkie testy zaliczone, dodajemy gracza do bazy
+					
+					if ($polaczenie->query("INSERT INTO uzytkownicy VALUES (NULL, '$nick', '$haslo_hash', '$email')"))
+					{
+						$_SESSION['udanarejestracja']=true;
+						header('Location: witamy.php');
+					}
+					else
+					{
+						throw new Exception($polaczenie->error);
+					}
+					
+				}
+				
+				$polaczenie->close();
+			}
+			
+		}
+		catch(Exception $e)
+		{
+			echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności i prosimy o rejestrację w innym terminie!</span>';
+			echo '<br />Informacja developerska: '.$e;
+		}
+		
+	}
+	
+	
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <title>movie4you - wypożyczalnia dvd/blu-ray</title>
+    <link href="bootstrap.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <script src='https://www.google.com/recaptcha/api.js'></script> <!-- recaptcha-zabezpieczenie przed botami masowe konta  -->
+    
+</head>
+<body>
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+              <li ><a href="index.php">home</a></li>
+              <li><a href="price.php">cennik</a></li>
+            <li><a href="base.php">katalog</a></li>
+                        <li><a href="search.php">szukaj</a></li>
+            <li><a href="contact.php">kontakt</a></li>
+            <li class="active"><a href="login.php">login/rejestracja</a></li>
+          </ul>
+        </nav>
+       <a href="index.php"><img class="logo" src="obrazki/logo.jpg" alt="Logo movie4you" /></a>
+      </div>
+	
+        <div class="jumbotron">
+	<form method="post">
+	<div class="form-register">
+		Nickname: <br /> <input type="text" value="<?php
+			if (isset($_SESSION['fr_nick']))
+			{
+				echo $_SESSION['fr_nick'];
+				unset($_SESSION['fr_nick']);
+			}
+		?>" name="nick" /><br />
+		
+		<?php
+			if (isset($_SESSION['e_nick']))
+			{
+				echo '<div class="error">'.$_SESSION['e_nick'].'</div>';
+				unset($_SESSION['e_nick']);
+			}
+		?>
+		
+		<br />
+		
+		<input type="submit" value="Zarejestruj się" />
+	</div>	
+	</form>
+ </div>
+	
+    <footer class="footer">
+		<p> movie4you © 2016 </p>
+    </footer>
+    </div> <!-- /container -->
+</body>
+</html>
