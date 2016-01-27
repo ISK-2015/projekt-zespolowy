4422246529c337a4d808c512372d14f42276cd86
hornowski92 <marcin_hornowski@wp.pl>
2016-01-27 22:51:54 +0100

    update

--- WEB/base.php
+++ WEB/base.php
@@ -6,6 +6,41 @@ $smarty = new Smarty();
 $smarty -> template_dir = 'html';
 $smarty -> compile_dir = 'temp';
 $smarty -> cache_dir = 'cache';
+
+require_once('connect.php');
+
+$connect = new mysqli($host,$db_user,$db_password,$db_name); //łšczenie z bazš, mysqli i-improved, @-wycisza błędy
+
+	if ($connect -> connect_errno!=0)
+	{
+		echo "Error: ".$connect->connect_errno; // 'error' a nie błšd, bo nie ma zadeklarowanego utf-8 i może sie krzaczyć
+	}
+	else
+	{
+			//zmiana kodowania na utf-8		
+	 $connect->set_charset("utf8");
+			
+	$query = "SELECT * FROM `tb_movies` WHERE 1 ORDER BY `id_movie`";
+
+		$wynikowa = array();
+		try {
+			$result   = $connect->query($query);
+
+
+		      	while($row = $result->fetch_array())
+			  {	
+				$tablica[]=$row;
+			  }
+
+		    }//obsługa błędu
+		    catch (PDOException $ex) { 
+				echo "błąd bazy danych";
+		    };
+
+}
+	
+$smarty -> assign('tb_movies', $tablica);
+
 $smarty -> display('base.html');
 
 ?>
